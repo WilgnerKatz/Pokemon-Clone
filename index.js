ab86f737c759626df40b553b10b2b1eff57ceabf
Lastepeck <wilgnerdutra@hotmail.com>
2022-06-20 09:58:59 -0700

    Added Player Movement and Collisions Array

--- index.js
+++ index.js
@@ -5,9 +5,11 @@ const context = canvas.getContext('2d');
 canvas.width = 1024;
 canvas.height = 576;
 
-
-context.fillStyle = 'white';
-context.fillRect(0, 0, canvas.width, canvas.height);
+//Collisions, it checks every 70 tiles
+const collisions
+for (let i = 0; i < collisions.length; i+= 70) {
+    collisions.slice(i, 70 + i)
+}
 
 
 const image = new Image();
@@ -17,22 +19,118 @@ const playerImage = new Image();
 playerImage.src = './Assets/playerDown.png';
 
 
-image.onload = () => {
-    context.drawImage(image, -300, -100);
+//Player Movement
+
+
+
+// Illusion of Movement
+class Sprite {
+    constructor({ position, velocity, image}) {
+        this.position = position
+        this.image = image
+    }
+    draw() {
+        context.drawImage(this.image, this.position.x, this.position.y);
+    }
+}
+
+const background = new Sprite ({ 
+    position: { 
+        x: -300, 
+        y: -100
+    },
+    image: image
+
+})
+
+
+const keys = {
+    w: {
+        pressed : false
+    }, 
+    s: {
+        pressed : false
+    },
+    a: {
+        pressed : false
+    },
+    d: {
+        pressed : false
+    }
+}
+function movement() {
+    window.requestAnimationFrame(movement);
+    background.draw()
     context.drawImage(
+
         //Cropping of the Image
         playerImage, 
         0, 
         0, 
         playerImage.width / 4, 
         playerImage.height,
+
         // Position 
         canvas.width / 2 - playerImage.width / 4, 
         canvas.height / 2 - playerImage.height / 2,
         playerImage.width / 4, 
         playerImage.height,
         )
+
+        // This allows for holding two different movement keys at the same time
+        if (keys.w.pressed && lastKey === 'w')  background.position.y += 3;
+        else if (keys.s.pressed && lastKey === 's')  background.position.y -= 3;
+        else if (keys.a.pressed && lastKey === 'a')  background.position.x += 3;
+        else if (keys.d.pressed && lastKey === 'd') background.position.x -= 3;
+        
 }
+movement();
 
 
-//Player Movement
+// WSAD 
+let lastKey = ''
+window.addEventListener('keydown', (event) => {
+    switch (event.key) {
+        case 'w':
+            keys.w.pressed = true;
+            lastKey = 'w'
+            break;
+
+        case 's':
+            keys.s.pressed = true;
+            lastKey = 's'
+            break;
+        
+        case 'a':
+            keys.a.pressed = true;
+            lastKey = 'a'
+            break;
+
+        case 'd':
+            keys.d.pressed = true;
+            lastKey = 'd'
+            break;
+    }
+    
+})
+
+window.addEventListener('keyup', (event) => {
+    switch (event.key) {
+        case 'w':
+            keys.w.pressed = false;
+            break;
+
+        case 's':
+            keys.s.pressed = false;
+            break;
+        
+        case 'a':
+            keys.a.pressed = false;
+            break;
+
+        case 'd':
+            keys.d.pressed = false;
+            break;
+    }
+    
+})
