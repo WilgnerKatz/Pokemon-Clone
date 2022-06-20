273c8377da7a8c3fbad27ce68c2f40235eb20b16
Lastepeck <wilgnerdutra@hotmail.com>
2022-06-20 12:41:44 -0700

    Boundaries almost done

--- index.js
+++ index.js
@@ -6,11 +6,43 @@ canvas.width = 1024;
 canvas.height = 576;
 
 //Collisions, it checks every 70 tiles
-const collisions
+const collisionsMap = []
 for (let i = 0; i < collisions.length; i+= 70) {
-    collisions.slice(i, 70 + i)
+    collisionsMap.push(collisions.slice(i, 70 + i));
+
 }
 
+class Boundary {
+    static width = 48
+    static height = 48
+    constructor ({position}) {
+        this.position = position 
+        this.width = 48;
+        this.height = 48;
+    }
+    draw () {
+      context.fillStyle = 'red'  
+      context.fillRect(this.position.x, this.position.y, this.width, this.height)
+    }
+}
+
+const boundaries = []
+const offset = {
+    x: -300, 
+    y: -100
+}
+
+collisionsMap.forEach((row, i) => {
+    row.forEach ((symbol, j) => {
+    if (symbol === 1025)
+     boundaries.push(
+        new Boundary({
+            position: {
+         x: j * Boundary.width + offset.x,
+         y: i * Boundary.height + offset.y
+     }}))   
+    })
+})
 
 const image = new Image();
 image.src = './Assets/map.png';
@@ -25,19 +57,53 @@ playerImage.src = './Assets/playerDown.png';
 
 // Illusion of Movement
 class Sprite {
-    constructor({ position, velocity, image}) {
+    constructor({ position, velocity, image, frames = {max: 1} }) {
         this.position = position
         this.image = image
+        this.frames = frames
+        this.image.onload = () => {
+            this.width = this.image.width / this.frames.max
+            this.height = this.image.height 
+        }
+        
     }
     draw() {
-        context.drawImage(this.image, this.position.x, this.position.y);
+        context.drawImage (
+
+            //Cropping of the Image
+            this.image, 
+            0, 
+            0, 
+            this.image.width / this.frames.max, 
+            this.image.height,
+    
+            // Position 
+            this.position.x,
+            this.position.y,
+            this.image.width / this.frames.max, 
+            this.image.height,
+            )
     }
 }
 
+
+
+const player = new Sprite({
+    position: {
+        x: canvas.width / 2 - 192 / 4 / 2, 
+        y: canvas.height / 2 - 68 / 2
+    },
+    image: playerImage, 
+    frames: {
+        max: 4
+    }
+})
+
+
 const background = new Sprite ({ 
     position: { 
-        x: -300, 
-        y: -100
+        x: offset.x, 
+        y: offset.y
     },
     image: image
 
@@ -58,30 +124,63 @@ const keys = {
         pressed : false
     }
 }
+
+
+
+const movables = [background, ...boundaries]
+
+function collision({rectangle1, rectangle2}) {
+return (
+    rectangle1.position.x + rectangle1.width >= rectangle2.position.x && 
+    rectangle1.position.x <= rectangle2.position.x + rectangle2.width && 
+    rectangle1.position.y <= rectangle2.position.y + rectangle2.height && 
+    rectangle1.position.y + rectangle1.height >= rectangle2.position.y)
+
+}
 function movement() {
     window.requestAnimationFrame(movement);
     background.draw()
-    context.drawImage(
+    boundaries.forEach(boundary => {
+      boundary.draw();  
+    })
+    player.draw()
 
-        //Cropping of the Image
-        playerImage, 
-        0, 
-        0, 
-        playerImage.width / 4, 
-        playerImage.height,
+        // This allows for holding two different movement keys at the same time, and also prevents certain objects from moving 
+        if (keys.w.pressed && lastKey === 'w') {
+        //Prevent Player From Colliding with boundary
+        for (let i = 0; i < boundaries.length; i++) {
+            const boundary = boundaries[i]
+            if (
+              collision({
+                rectangle1: player,
+                rectangle2: {
+                  ...boundary,
+                  position: {
+                    x: boundary.position.x,
+                    y: boundary.position.y - 3
+                  }
+                }
+              })
+            ) {
+                console.log('colliding');
+            }
+          }
+            movables.forEach((movable) => {
+                movable.position.y +=3
+            })
 
-        // Position 
-        canvas.width / 2 - playerImage.width / 4, 
-        canvas.height / 2 - playerImage.height / 2,
-        playerImage.width / 4, 
-        playerImage.height,
-        )
-
-        // This allows for holding two different movement keys at the same time
-        if (keys.w.pressed && lastKey === 'w')  background.position.y += 3;
-        else if (keys.s.pressed && lastKey === 's')  background.position.y -= 3;
-        else if (keys.a.pressed && lastKey === 'a')  background.position.x += 3;
-        else if (keys.d.pressed && lastKey === 'd') background.position.x -= 3;
+          } else if (keys.s.pressed && lastKey === 's')  {
+            movables.forEach((movable) => {
+            movable.position.y -=3
+        })
+        } else if (keys.a.pressed && lastKey === 'a')  {
+            movables.forEach((movable) => {
+            movable.position.x +=3
+        })
+        } else if (keys.d.pressed && lastKey === 'd') {movables.forEach((movable) => {
+            movable.position.x -=3
+        })
+    }
         
 }
 movement();
