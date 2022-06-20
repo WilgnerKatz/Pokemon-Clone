2f9bb09e6c9b7dfc8bed0438988e3f8355cb1ab5
Lastepeck <wilgnerdutra@hotmail.com>
2022-06-20 13:56:54 -0700

    Character Sprite Changes Direction

--- index.js
+++ index.js
@@ -12,19 +12,7 @@ for (let i = 0; i < collisions.length; i+= 70) {
 
 }
 
-class Boundary {
-    static width = 48
-    static height = 48
-    constructor ({position}) {
-        this.position = position 
-        this.width = 48;
-        this.height = 48;
-    }
-    draw () {
-      context.fillStyle = 'rgba(255, 0, 0, 0.0)'  
-      context.fillRect(this.position.x, this.position.y, this.width, this.height)
-    }
-}
+
 
 const boundaries = []
 const offset = {
@@ -47,45 +35,17 @@ collisionsMap.forEach((row, i) => {
 const image = new Image();
 image.src = './Assets/map.png';
 
-const playerImage = new Image();
-playerImage.src = './Assets/playerDown.png';
+const playerDownImage = new Image();
+playerDownImage.src = './Assets/playerDown.png';
 
+const playerUpImage = new Image();
+playerUpImage.src = './Assets/playerUp.png';
 
-//Player Movement
+const playerLeftImage = new Image();
+playerLeftImage.src = './Assets/playerLeft.png';
 
-
-
-// Illusion of Movement
-class Sprite {
-    constructor({ position, velocity, image, frames = {max: 1} }) {
-        this.position = position
-        this.image = image
-        this.frames = frames
-        this.image.onload = () => {
-            this.width = this.image.width / this.frames.max
-            this.height = this.image.height 
-        }
-        
-    }
-    draw() {
-        context.drawImage (
-
-            //Cropping of the Image
-            this.image, 
-            0, 
-            0, 
-            this.image.width / this.frames.max, 
-            this.image.height,
-    
-            // Position 
-            this.position.x,
-            this.position.y,
-            this.image.width / this.frames.max, 
-            this.image.height,
-            )
-    }
-}
-
+const playerRightImage = new Image();
+playerRightImage.src = './Assets/playerRight.png';
 
 
 const player = new Sprite({
@@ -93,9 +53,15 @@ const player = new Sprite({
         x: canvas.width / 2 - 192 / 4 / 2, 
         y: canvas.height / 2 - 68 / 2
     },
-    image: playerImage, 
+    image: playerDownImage, 
     frames: {
         max: 4
+    },
+    sprites: {
+        up: playerUpImage, 
+        left: playerLeftImage, 
+        down: playerDownImage,
+        right: playerRightImage
     }
 })
 
@@ -147,7 +113,10 @@ function movement() {
 
         // This allows for holding two different movement keys at the same time, and also prevents certain objects from moving, and also prevents Player From Colliding with boundary
         let moving = true;
+        player.moving = false;
         if (keys.w.pressed && lastKey === 'w') {
+        player.moving = true;    
+        player.image = player.sprites.up
         for (let i = 0; i < boundaries.length; i++) {
             const boundary = boundaries[i]
             if (
@@ -173,6 +142,8 @@ function movement() {
             })
 
           } else if (keys.s.pressed && lastKey === 's')  {
+            player.moving = true;
+            player.image = player.sprites.down
             for (let i = 0; i < boundaries.length; i++) {
                 const boundary = boundaries[i]
                 if (
@@ -197,6 +168,8 @@ function movement() {
             movable.position.y -=3
         })
         } else if (keys.a.pressed && lastKey === 'a')  {
+            player.moving = true;
+            player.image = player.sprites.left
             for (let i = 0; i < boundaries.length; i++) {
                 const boundary = boundaries[i]
                 if (
@@ -221,6 +194,8 @@ function movement() {
             movable.position.x +=3
         })
         } else if (keys.d.pressed && lastKey === 'd') {
+            player.moving = true;
+            player.image = player.sprites.right
             for (let i = 0; i < boundaries.length; i++) {
                 const boundary = boundaries[i]
                 if (
