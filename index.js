e412c4bddc69c59b0db3179f4b7109f4a6df8bed
Lastepeck <wilgnerdutra@hotmail.com>
2022-06-20 08:40:58 -0700

    Player Added in the Map

--- index.js
+++ index.js
@@ -13,8 +13,26 @@ context.fillRect(0, 0, canvas.width, canvas.height);
 const image = new Image();
 image.src = './Assets/map.png';
 
+const playerImage = new Image();
+playerImage.src = './Assets/playerDown.png';
 
 
 image.onload = () => {
     context.drawImage(image, -300, -100);
+    context.drawImage(
+        //Cropping of the Image
+        playerImage, 
+        0, 
+        0, 
+        playerImage.width / 4, 
+        playerImage.height,
+        // Position 
+        canvas.width / 2 - playerImage.width / 4, 
+        canvas.height / 2 - playerImage.height / 2,
+        playerImage.width / 4, 
+        playerImage.height,
+        )
 }
+
+
+//Player Movement
