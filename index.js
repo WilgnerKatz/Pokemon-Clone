7ded54b2a8b6b6338aab2671fa1be38767e12cad
Lastepeck <wilgnerdutra@hotmail.com>
2022-06-20 08:11:22 -0700

    Game Map Added

--- index.js
+++ index.js
@@ -9,3 +9,12 @@ canvas.height = 576;
 context.fillStyle = 'white';
 context.fillRect(0, 0, canvas.width, canvas.height);
 
+
+const image = new Image();
+image.src = './Assets/map.png';
+
+
+
+image.onload = () => {
+    context.drawImage(image, -300, -100);
+}
